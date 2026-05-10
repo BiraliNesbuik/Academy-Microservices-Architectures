f8b203a88354bac0fca3eb5933be34aadbd7818a
BiraliNesbuik <emirtek98@gmail.com>
2026-05-10 20:29:24 +0300

    CORS'a geliştirme portları eklendi, Vite sabit porta alındı

--- dispatcher/main.py
+++ dispatcher/main.py
@@ -67,7 +67,7 @@ Instrumentator().instrument(app).expose(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
